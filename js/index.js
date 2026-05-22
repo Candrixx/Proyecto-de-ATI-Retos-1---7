f6f99da33d5c7eea80e45681118772d741d11127
Carlos Arena <carlosarenaph@gmail.com>
2026-05-21 20:45:16 -0400

    listado de perfiles dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 function getConfig() {
+
+    //PARTE 1 logo, barra de busqueda
     const tituloH1 = document.querySelector('.logo h1');
     const subTextoSpan = document.querySelector('.logo h1 span');
 
@@ -24,15 +26,45 @@ function getConfig() {
         botonBusqueda.textContent = config.search;
     }
 
+    //PARTE 2 semestre
     const tituloSeccion = document.querySelector('.title-section h2');
     if(tituloSeccion){
         tituloSeccion.textContent = config.semester;
     }
 
+
+    //PARTE 3 copyright
     const textoCopyright = document.querySelector('footer p');
     if(textoCopyright){
         textoCopyright.textContent = config.copyRight;
     }
+
+    //PARTE 4 tarjetas
+    const contenedorPrincipal = document.getElementById('contenedor-cartas');
+    
+    if (!contenedorPrincipal) return;
+
+    contenedorPrincipal.innerHTML = "";
+
+    profiles.forEach(perfil => {
+        const tarjeta = document.createElement('div');
+        tarjeta.className = 'card';
+        tarjeta.innerHTML = `
+            <div class="card-header">
+                <img src="${perfil.ci}/${perfil.ci}Big${perfil.image_ext}" alt="${perfil.ci}">
+            </div>
+            <div class="card-footer">
+                <p>${perfil.name}</p>
+            </div>
+            <div class="blue-bar"></div>
+        `;
+
+        tarjeta.addEventListener('click', () => {
+            window.location.href = `profile.html?cedula=${perfil.ci}`;
+        });
+
+        contenedorPrincipal.appendChild(tarjeta);
+    });
 }
 
 getConfig();
